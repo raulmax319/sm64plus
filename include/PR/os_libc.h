7d7b8567cfa70190eea3ce289316a2d120a779b3
Francesco Scotti <francesco.scotti92@gmail.com>
2021-12-18 03:20:12 +0100

    avoid bcopy bzero declarations macos

--- include/PR/os_libc.h
+++ include/PR/os_libc.h
@@ -3,8 +3,10 @@
 
 #include "ultratypes.h"
 
+#ifndef TARGET_MACOS
 // Old deprecated functions from strings.h, replaced by memcpy/memset.
 extern void bcopy(const void *, void *, size_t);
 extern void bzero(void *, size_t);
+#endif
 
 #endif /* !_OS_LIBC_H_ */
