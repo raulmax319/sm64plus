edfeb56ccd00d92c38df05fb0e118c208bf0c8bc
Raul Max <17752968+raulmax319@users.noreply.github.com>
2024-08-30 09:06:18 -0300

    fix: add missing functions definitions

--- src/game/camera.h
+++ src/game/camera.h
@@ -677,6 +677,9 @@ extern u8 gRecentCutscene;
 
 // TODO: sort all of this extremely messy shit out after the split
 
+void manual_cam_modes(struct Camera *c);
+void mario_cam_modes(struct Camera *c);
+void lakitu_cam_modes(struct Camera *c);
 void set_camera_shake_from_hit(s16 shake);
 void set_environmental_camera_shake(s16 shake);
 void set_camera_shake_from_point(s16 shake, f32 posX, f32 posY, f32 posZ);
